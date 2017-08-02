dfaee3b9f4a5b63d8926fa0fa572b41afb7bbc8f
derpyfrs <simon_chanct@hotmail.com>
2017-08-02 06:14:32 +0000

    finished adding short url and long url codes

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
 
 var urlDatabase = {
@@ -9,8 +12,22 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRandomNumber() {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for(var i = 0; i < 6; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  if (urlDatabase.hasOwnProperty(text)) {
+    this.generateRandomNumber();
+  }
+  return text;
+}
+
+console.log(urlDatabase);
+
 app.get("/", (req, res) => {
-  res.end("Hello!");
+  res.redirect('/urls');
 });
 
 app.listen(PORT, () => {
@@ -27,7 +44,29 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
+});
+
+
+app.post("/urls", (req, res) => {
+  console.log('this is', req.body);  // debug statement to see POST parameters
+  var shortUrl = generateRandomNumber();
+  urlDatabase[shortUrl] = req.body.longURL;    // Respond with 'Ok' (we will replace this)
+  console.log(urlDatabase);
+  res.send(`Ok use http://localhost:8080/u/${shortUrl}`);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL];
+  // console.log('res body', res.body)
+  // console.log('red body', req.params)
+  res.status(301).redirect(`https://wwww.${longURL}`);
+  console.log(longURL)
+
 });
